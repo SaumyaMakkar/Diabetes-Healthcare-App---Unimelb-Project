14d34a13ff043e3ccd9f600b3ca77628a34d8487
Jean Vera Soto <jeanvs23@gmail.com>
2022-03-24 10:05:38 +1100

    updated about diabetes page

--- assets/js/wt.js
+++ assets/js/wt.js
@@ -1,8 +1,8 @@
 
-function openModal(modalName) {
+function openModal(modalId) {
     console.log("iopenModal");
     // Get the modal
-    var modal = document.getElementById(modalName);
+    var modal = document.getElementById(modalId);
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
